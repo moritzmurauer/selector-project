0153b3cbbdf01b2b473a44b9349d7bee19178b1c
moritzmurauer <moritzskaten@googlemail.com>
2023-01-08 11:49:31 +0800

    feat(app): setup general app component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
 <template>
+  <Navbar />
   <div>
-    <router-view/>
+    <router-view :key="route.path" />
   </div>
 </template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+import { useRoute } from "vue-router";
+
+export default {
+  components: { Navbar },
+
+  setup() {
+    const route = useRoute();
+    return { route };
+  },
+};
+</script>
+<style></style>
