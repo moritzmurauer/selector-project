eaed0bc62f187b4f46166b788f6d05a6b890798e
moritzmurauer <moritzskaten@googlemail.com>
2023-02-23 13:06:17 +0800

    feat(explore): update explore page design

--- src/views/Explore.vue
+++ src/views/Explore.vue
@@ -3,31 +3,36 @@
     <!-- A list of the top 10 contributors who posted the most spots yet -->
 
     <h1 class="text-xl mt-10">Explore all users</h1>
-    <div v-for="user in users" :key="user.id">
-      <div class="card mt-2 p-1">
-        <div class="d-flex mt-1 grid">
-          <div class="d-flex">
-            <div class="pr-1 " v-if="users">
-              <img
-                class="avatar avatar-user"
-                v-if="user.avatarUrl"
-                :src="user.avatarUrl"
-              />
+    <div
+      class="container mx-auto grid
+sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 pt-8 gap-16"
+    >
+      <div v-for="user in users" :key="user.id">
+        <router-link :to="{ name: 'UserPosts', params: { id: user.id } }">
+          <div class="mt-2 p-1">
+            <div class="d-flex">
+              <div
+                class="pr-1 cover transform transition duration-300 hover:scale-110"
+                v-if="users"
+              >
+                <img
+                  class="rounded object-cover object-center"
+                  v-if="user.avatarUrl"
+                  :src="user.avatarUrl"
+                />
 
+                <img
+                  class="object-cover object-center"
+                  v-if="!user.avatarUrl"
+                  src="@/assets/default.png"
+                />
+              </div>
               <div>
                 {{ user.displayName }}
               </div>
-              <img
-                class="w-60 h-60"
-                v-if="!user.avatarUrl"
-                src="@/assets/default.png"
-              />
-            </div>
-            <div>
-              {{ user.style }}
             </div>
           </div>
-        </div>
+        </router-link>
       </div>
     </div>
   </div>
@@ -48,4 +53,30 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.cover {
+  overflow: hidden;
+  border-radius: 20px;
+  position: relative;
+  padding: 100px;
+  max-height: 100px;
+}
+.cover img {
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  min-width: 100%;
+  min-height: 100%;
+  max-width: 100%;
+  height: auto;
+}
+
+.transform {
+  transform: scale(100%);
+}
+
+.scale-110 {
+  transform: scale(110%);
+}
+</style>
