59cfacad22967b5e5712abedce3abefa3962a6d5
moritzmurauer <moritzskaten@googlemail.com>
2023-02-23 13:04:00 +0800

    feat(list): create listing component for artworks

--- src/components/ListView.vue
+++ src/components/ListView.vue
@@ -1,20 +1,18 @@
 <template>
   <div
-    class="container mx-auto grid
-sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 px-32 pt-8 gap-2"
+    class="content mx-20px grid
+sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3  pt-8 gap-2"
   >
     <div v-for="post in posts" :key="post.id">
       <router-link :to="{ name: 'PostDetail', params: { id: post.id } }">
         <div class="single">
           <div class="thumbnail">
-            <img class="h-96" :src="post.coverUrl" />
+            <img class="h-80 object-cover object-center" :src="post.coverUrl" />
           </div>
           <router-link :to="{ name: 'UserPosts', params: { id: post.userId } }">
             <div>
-              <p>{{ post.userName }}</p>
-            </div>
-            <div>
-              {{ post.category }}
+              <h2>{{ post.title }}</h2>
+              <p class="text-sm">by {{ post.userName }}</p>
             </div>
           </router-link>
         </div>
@@ -29,4 +27,14 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.thumbnail {
+  overflow: hidden;
+}
+
+.thumbnail img {
+  min-width: 100%;
+  min-height: 100%;
+  max-width: 100%;
+}
+</style>
