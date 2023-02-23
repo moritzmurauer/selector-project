bb8fe076f70d1fbda8bba5d2b92e7aa6e1bc86f6
moritzmurauer <moritzskaten@googlemail.com>
2023-02-23 13:07:53 +0800

    feat(login): add loading animation

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,40 +1,54 @@
 <template>
-<Navbar />
-  <form @submit.prevent="handleSubmit">
-    <h3>Admin Login</h3>
-    <input type="email" placeholder="Email" v-model="email">
-    <input type="password" placeholder="Password" v-model="password">
+  <form class="mt-16" @submit.prevent="handleSubmit">
+    <h3 class="text-center text-2xl">Login</h3>
+    <input
+      class="bg-white mt-5 focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 pb-3 px-4 block w-full appearance-none leading-normal"
+      type="email"
+      placeholder="Email"
+      v-model="email"
+    />
+    <input
+      class="bg-white mt-5 focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 pb-3 px-4 block w-full appearance-none leading-normal"
+      type="password"
+      placeholder="Password"
+      v-model="password"
+    />
     <div v-if="error" class="error">{{ error }}</div>
-    <button v-if="!isPending">Log in</button>
-    <button v-if="isPending" disabled>Loading</button>
+    <button class="w-full btn-full rounded mt-5" v-if="!isPending">
+      Log in
+    </button>
+    <div v-else disabled class="w-full flex justify-center mt-5">
+      <span class="save-icon">
+        <span class="loader"></span>
+        <span class="loader"></span>
+        <span class="loader"></span>
+      </span>
+    </div>
   </form>
 </template>
 
 <script>
-
 // login for admins of the page
-import useLogin from '@/composables/useLogin'
-import Navbar from '@/components/Navbar.vue'
-import { ref } from 'vue'
-import { useRouter } from 'vue-router'
+import useLogin from "@/composables/useLogin";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
 
 export default {
-  components: {Navbar},
   setup() {
-    const { error, login, isPending } = useLogin()
-    const router = useRouter()
+    const { error, login, isPending } = useLogin();
+    const router = useRouter();
 
-    const email = ref('')
-    const password = ref('')
+    const email = ref("");
+    const password = ref("");
 
     const handleSubmit = async () => {
-      const res = await login(email.value, password.value)
+      const res = await login(email.value, password.value);
       if (!error.value) {
-        router.push({ name: 'Home' })
+        router.push({ name: "Home" });
       }
-    }
-    
-    return { email, password, handleSubmit, error, isPending }
-  }
-}
+    };
+
+    return { email, password, handleSubmit, error, isPending };
+  },
+};
 </script>
