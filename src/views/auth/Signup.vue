e59459551bf7d00ebebfc1addb4a4069b1023dd1
moritzmurauer <moritzskaten@googlemail.com>
2023-02-23 13:08:08 +0800

    feat(signup): add loading animation

--- src/views/auth/Signup.vue
+++ src/views/auth/Signup.vue
@@ -1,16 +1,35 @@
 <template>
-  <Navbar />
   <div class="bg">
     <div class="background-cliff">
-      <form class="mt-4 space-y-12 formblock" @submit.prevent="handleSubmit">
-        <h3>Sign up</h3>
-        <input type="text" v-model="displayName" placeholder="Name" />
-        <input type="email" v-model="email" placeholder="Email" />
-        <input type="password" v-model="password" placeholder="password" />
+      <form
+        class="mt-4 space-y-12 formblock shadow-md"
+        @submit.prevent="handleSubmit"
+      >
+        <h2 class="text-center text-2xl">Sign up</h2>
+        <input
+          class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 pb-3 px-4 block w-full appearance-none leading-normal"
+          type="text"
+          v-model="displayName"
+          placeholder="Name"
+        />
+        <input
+          class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 pb-3 px-4 block w-full appearance-none leading-normal"
+          type="email"
+          v-model="email"
+          placeholder="Email"
+        />
+        <input
+          class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 pb-3 px-4 block w-full appearance-none leading-normal"
+          type="password"
+          v-model="password"
+          placeholder="password"
+        />
         <div class="mt-2 mb-5">
-          <h4>Please select a category</h4>
+          <h4 class="text-center">
+            Please select in which category your art is
+          </h4>
           <select
-            class="mb-1 mt-1 pl-3 p-3 pt-1 pb-1 outline-primary"
+            class="mb-1 mt-1 pl-3 p-3 pt-1 pb-1 border rounded-lg border-primary-400 w-full"
             v-model="style"
             placeholder="pick here"
           >
@@ -24,12 +43,19 @@
               {{ option.text }}
             </option>
           </select>
-          <p>Category: {{ style }}</p>
         </div>
         <div class="error" v-if="error">{{ error }}</div>
 
-        <button class="btn outline" v-if="!isPending">Sign up</button>
-        <button v-if="isPending" disabled>Loading...</button>
+        <button class="w-full btn-full rounded mt-5" v-if="!isPending">
+          Sign up
+        </button>
+        <div v-else disabled class="w-full flex justify-center mt-5">
+          <span class="save-icon">
+            <span class="loader"></span>
+            <span class="loader"></span>
+            <span class="loader"></span>
+          </span>
+        </div>
 
         <p class="mt-1 d-flex justify-content-center align-center">
           Already have an Account? Log in&nbsp;<router-link
@@ -43,13 +69,11 @@
 </template>
 
 <script>
-import Navbar from "@/components/Navbar.vue";
 import useSignup from "@/composables/useSignup";
 import { ref } from "vue";
 import { useRouter } from "vue-router";
 
 export default {
-  components: { Navbar },
   setup() {
     const { error, signup, isPending } = useSignup();
     const router = useRouter();
@@ -58,12 +82,14 @@ export default {
     const email = ref("");
     const password = ref("");
     const displayName = ref("");
+    const grid = ref("Triple Grid");
 
     // Defines possible Category Inputs
     const options = ref([
       { text: "Photography", value: "A" },
       { text: "Drawings", value: "B" },
       { text: "Illustration", value: "C" },
+      { text: "Diverse", value: "D" },
     ]);
 
     const style = ref("");
@@ -73,10 +99,11 @@ export default {
         email.value,
         password.value,
         displayName.value,
+        grid.value,
         style.value
       );
       if (!error.value) {
-        router.push({ name: "Home" });
+        router.push({ name: "UserPosts", params: { id: res.user.uid } });
       }
     };
 
@@ -90,6 +117,7 @@ export default {
       isPending,
       options,
       style,
+      grid,
     };
   },
 };
@@ -102,9 +130,5 @@ export default {
 
 .underline-link {
   text-decoration: underline;
-}
-
-.formblock {
-  box-shadow: 4px 6px 4px 4px rgba(50, 50, 50, 0.2);
 }
 </style>
